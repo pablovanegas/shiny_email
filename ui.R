6723448561a9dd9723739f2e230623fadcae5bd7
Juan <juvanegas@unal.edu.co>
2024-03-26 02:04:14 -0500

    add .txt file and webRbundle settle
    Up to modifications

--- ui.R
+++ ui.R
@@ -1,16 +1,20 @@
 library(shiny)
-library(readxl)
+library(readr)
 library(stringr)
 
 ui <- fluidPage(
+  
   titlePanel("File Upload"),
+  
   sidebarLayout(
+    
     sidebarPanel(
-      fileInput("file", "Choose a File", accept = c("text/csv", "text/xlsx", "text/sql", "text/xml", ".csv", ".xlsx", ".sql", ".xml", ".txt"))
-    ),
+      fileInput("file", "Choose a File", accept = c("text/csv", "text/xlsx", "text/sql", "text/xml", ".csv", ".xlsx", ".sql", ".xml", ".txt")),
+      downloadButton("Download", "Download Document")
+    ), # End of sidebarPanel
+    # Show the content of the file
     mainPanel(
-      tableOutput("contents"),
-      textOutput("columnNames")
-    )
-  )
-)
+      tableOutput("contents")
+    ) # End of mainPanel
+  ) # End of sidebarLayout
+)  # End of fluidPage
