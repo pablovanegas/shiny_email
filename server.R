726be5504eec16e78f27efbca83177bf22d3eeed
pablovanegas[credential] <juvanegas@unal.edu.co>
2024-04-04 21:55:28 -0500

    Mejora
    manejo de extensiones

--- server.R
+++ server.R
@@ -3,6 +3,7 @@ library(readxl)
 library(stringr)
 
 server <- function(input, output) {
+  
   # Store the original file content
   originalFileContent <- reactiveVal(NULL)
   
@@ -10,7 +11,35 @@ server <- function(input, output) {
   observeEvent(input$file, {
     req(input$file) # Read the file content
     
-    data_sheet <- read_excel(input$file$datapath)
+    # Determine the file type
+    file_type <- switch(input$file_type,
+                        "CSV" = "csv",
+                        "XLSX" = "xlsx",
+                        "SQL" = "sql",
+                        "XML" = "xml",
+                        "TXT" = 'txt')
+    
+    # Read the file based on its type
+    data_sheet <- switch(file_type,
+                         "csv" = read.csv(input$file$datapath, sep = input$csv_sep),  # Use the user-specified delimiter for CSV files
+                         "xlsx" = read_xlsx(input$file$datapath, sheet = input$excel_sheet),  # Use the user-specified sheet for Excel files
+                         "txt" = read.table(input$file$datapath, sep = input$txt_sep),  # Use the user-specified delimiter for TXT files
+                         "sql" = read_sql(input$file$datapath),  # No adjustments needed for SQL files
+                         "xml" = read_xml(input$file$datapath))  # No adjustments needed for XML files
+
+    # Verify the file extension
+    file_ext <- tools::file_ext(input$file$name)
+    
+    # Check if the file extension is valid
+    if (tolower(file_ext) != tolower(input$file_type)) {
+      # If not, show an error message and stop the execution
+      showModal(modalDialog(
+        title = "Error",
+        "The file extension does not match the selected file type. Please select the correct file type or upload a file with the correct extension.",
+        easyClose = TRUE
+      ))
+      return()  # Use return() instead of stop()
+    }
     
     # Create checkbox group
     output$checkbox_group <- renderUI({
